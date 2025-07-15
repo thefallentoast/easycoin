b0f659b823f7ff8f18ce68f55389f7962d95ee98
thefallentoast <113140368+thefallentoast@users.noreply.github.com>
2025-07-15 23:20:31 +0000

    Started main server work and added a basic template for it.

--- server_code/server.py
+++ server_code/server.py
@@ -1,4 +1,8 @@
-from miner import Miner
-# Note: Make sure to put the .so/.dll file in the same directory as miner.py!
 from protocol import ProtocolHandler
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html") # Change later
