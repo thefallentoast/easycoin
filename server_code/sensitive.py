e25a1929905efe83a0b77c285db58574ae69870d
thefallentoast <113140368+thefallentoast@users.noreply.github.com>
2025-07-16 12:31:10 +0000

    A little bit of work on server.py and started mysql "bridge".

---
+++ server_code/sensitive.py
@@ -0,0 +1,9 @@
+
+
+def get_sensitive_info():
+    return {
+        "host": "localhost",
+        "user": "root",
+        "password": "",
+        "database": "USERS"
+    } # Set YOUR information here!
--- server_code/server.py
+++ server_code/server.py
@@ -1,8 +1,35 @@
 from protocol import ProtocolHandler
-from flask import Flask, render_template
+from sensitive import get_sensitive_info
+import mysql.connector
 
-app = Flask(__name__)
+class UserDatabaseBridge:
+    '''
+    Bridge between user access (from the server) and the MariaDB database.
+    '''
+    def __init__(self, connection_info: dict[str, str, str, str]) -> None:
+        '''
+        Initialize the bridge, and connect to the database.
+        Arguments:
+            connection_info: dict with 4 str's
+                The information for connecting to the database. It should have the keys
+                "host" (typically set to "localhost"), "user" (typically root or some 
+                other username), "password" (the password for connecting), and 
+                (optional) "database" (the database name, not required if there's only
+                one in the server.)
+        '''
+        self.connection_info = connection_info
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html") # Change later
+        self.conn = mysql.connector.connect(**self.connection_info)
+        self.cursor = self.conn.cursor()
+    #TODO
+
+class Orchestrator:
+    def __init__(self, address: str = "0.0.0.0", port: int = 5870) -> None:
+        '''
+        Initializes the orchestrator server.
+        '''
+        self.address, self.port = address, port
+        
+        
+        self.bridge = UserDatabaseBridge(get_sensitive_info())
+        self.protocol_handler = ProtocolHandler(self.address, self.port)
